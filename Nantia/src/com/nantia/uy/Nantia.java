e4fa23d80cc873973d781499b4c2d38391b34232
emiben <emiben1@hotmail.com>
2015-02-06 21:09:32 -0200

    cambio 1

--- Nantia/src/com/nantia/uy/Nantia.java
+++ Nantia/src/com/nantia/uy/Nantia.java
@@ -2,4 +2,15 @@ package com.nantia.uy;
 
 public class Nantia {
 
+	private int version;
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	
+	
 }
